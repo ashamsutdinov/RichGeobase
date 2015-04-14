a66adea06dd60cfe6626845903eda43f901223a7
Aydar Shamsutdinov <aydarrrio@gmail.com>
2015-04-14 23:13:14 +0300

    Convertor signatures

--- Common.Libs/Common/Utils/DuplexObjectConverter.cs
+++ Common.Libs/Common/Utils/DuplexObjectConverter.cs
@@ -3,8 +3,8 @@ using Common.Contracts;
 
 namespace Common.Utils
 {
-    public class DuplexObjectConverter<TInterface1, TInterface2> :
-        IDuplexObjectConverter<TInterface1, TInterface2>
+    public class DuplexObjectConverter<TObject1, TObject2> :
+        IDuplexObjectConverter<TObject1, TObject2>
     {
         #region Private
 
@@ -12,19 +12,19 @@ namespace Common.Utils
 
         #endregion
 
-        public TInterface1 Convert(TInterface2 interface2)
+        public TObject1 Convert(TObject2 interface2)
         {
-            return _objectMapper.Map<TInterface2, TInterface1>(interface2);
+            return _objectMapper.Map<TObject2, TObject1>(interface2);
         }
 
-        public TInterface2 Convert(TInterface1 interface1)
+        public TObject2 Convert(TObject1 interface1)
         {
-            return _objectMapper.Map<TInterface1, TInterface2>(interface1);
+            return _objectMapper.Map<TObject1, TObject2>(interface1);
         }
     }
 
-    public class DuplexObjectConverter<TObject1, TInterface1, TInterface2> :
-        IDuplexObjectConverter<TInterface1,TInterface2>
+    public class DuplexObjectConverter<TObject1, TInterface1, TObject2> :
+        IDuplexObjectConverter<TInterface1,TObject2>
         where TObject1 : class, TInterface1
     {
         #region Private
@@ -35,15 +35,15 @@ namespace Common.Utils
 
         #region Implementation of IDuplexObjectConverter
 
-        public TInterface1 Convert(TInterface2 interface2)
+        public TInterface1 Convert(TObject2 interface2)
         {
-            return _objectMapper.Map<TInterface2, TObject1>(interface2);
+            return _objectMapper.Map<TObject2, TObject1>(interface2);
         }
 
-        public TInterface2 Convert(TInterface1 interface1)
+        public TObject2 Convert(TInterface1 interface1)
         {
             var obj1 = interface1 as TObject1;
-            return _objectMapper.Map<TObject1, TInterface2>(obj1);
+            return _objectMapper.Map<TObject1, TObject2>(obj1);
         }
 
         #endregion
